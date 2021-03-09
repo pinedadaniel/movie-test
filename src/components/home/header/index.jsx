aa06a2b309c825eda073bbd1428f62906a5acf2f
cartapro <cartakun@gmail.com>
2021-03-09 15:06:17 -0500

    creacion de vistas home y details

---
+++ src/components/home/header/index.jsx
@@ -0,0 +1,29 @@
+import './style.css';
+import Netflix from '../../../images/netflix.svg';
+function Header() {
+
+    return(
+        <div className="containerHeader">
+            <div className="header-left">
+            <img  className="cursor" src={Netflix} alt="" height="45px" width="145px"/>  
+            </div>
+            
+            <div className="header-right">
+                <select name="select-lang" id="select-lang" className="btn-lang">
+                
+                    <option value="Español">
+                    <span class="material-icons">
+language
+</span> Español</option>
+                    <option value="Ingles">Ingles</option>
+                </select>
+            <button className="btn-login cursor">Iniciar Sesion</button>
+            </div>
+            
+
+            
+        </div>
+    );
+    
+}
+export default Header;
